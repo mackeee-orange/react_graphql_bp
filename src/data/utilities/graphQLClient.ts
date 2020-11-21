592b18470bde904dd37eaf500e569def4b143149
mackeee-orange <makino.aaa@gmail.com>
2020-11-21 21:40:39 +0900

    useCurrentAccountを実装

--- src/data/utilities/graphQLClient.ts
+++ src/data/utilities/graphQLClient.ts
@@ -2,15 +2,9 @@ import { ApolloClient, ApolloLink, HttpLink, InMemoryCache } from "@apollo/clien
 import { API_HOST_URL } from "commons/constants/url";
 import { onError } from "@apollo/client/link/error";
 import { createFragmentArgumentLink } from "apollo-link-fragment-argument";
-import routes from "commons/constants/routes";
-import PersistenceKeys from "commons/constants/persistenceKeys";
+import signOut from "../../commons/functions/signOut";
 
 const cache = new InMemoryCache();
-
-function logout(): void {
-  localStorage.setItem(PersistenceKeys.AUTH_TOKEN, "");
-  window.location.href = routes.signIn();
-}
 
 const httpLink = new HttpLink({ uri: `${API_HOST_URL}/graphql` });
 const fragmentLink = createFragmentArgumentLink();
@@ -30,7 +24,7 @@ const errorLink = onError(({ graphQLErrors, networkError, operation }) => {
               Authorization: null,
             },
           });
-          logout();
+          signOut();
       }
     }
   }
